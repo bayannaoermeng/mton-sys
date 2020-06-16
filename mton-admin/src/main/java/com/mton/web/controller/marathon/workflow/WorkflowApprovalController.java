9893f308b6a0dca9138bb0e52f088a4b7f48127a
cuiguangqiang <cuiguangqiang@hyjf.com>
2020-06-16 17:37:29 +0800

    流程列表

--- mton-admin/src/main/java/com/mton/web/controller/marathon/workflow/WorkflowApprovalController.java
+++ mton-admin/src/main/java/com/mton/web/controller/marathon/workflow/WorkflowApprovalController.java
@@ -1,7 +1,13 @@
 package com.mton.web.controller.marathon.workflow;
 
+import com.mton.common.base.AjaxResult;
+import com.mton.framework.web.base.BaseController;
+import com.mton.system.domain.SysUser;
+import com.mton.system.service.ISysRoleService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * @author cuiguangqiang
@@ -10,8 +16,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
  */
 @RequestMapping("/approval")
 @Controller
-public class WorkflowApprovalController {
+public class WorkflowApprovalController extends BaseController {
 
-//    public String
+    @Autowired
+    private ISysRoleService sysRoleService;
+
+
+    /**
+     * 审批列表
+     * @return
+     */
+    @RequestMapping("list")
+    @ResponseBody
+    public AjaxResult list(){
+
+        SysUser sysUser = super.getSysUser();
+
+
+
+        return AjaxResult.success();
+    }
+
 
 }
